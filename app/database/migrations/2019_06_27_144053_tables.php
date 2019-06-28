0fe6d46a3f9cab3f6f9d21f97087653073017cfb
Daniel Guilmezian <dguilmezian@gmail.com>
2019-06-27 22:46:12 -0300

    agregados nulls

--- app/database/migrations/2019_06_27_144053_tables.php
+++ app/database/migrations/2019_06_27_144053_tables.php
@@ -16,25 +16,25 @@ class Tables extends Migration
         Schema::create('categoria', function (Blueprint $table) {
             $table->increments('idcategoria');
             $table->string('nombre');
-            $table->string('descripcion');
-            $table->tinyInteger('condicion');
+            $table->string('descripcion')->nullable();
+            $table->tinyInteger('condicion')->nullable();
         });
 
         Schema::create('categoria_persona', function (Blueprint $table) {
             $table->increments('idcategoria_persona');
-            $table->string('nombre');
+            $table->string('nombre')->nullable();
         });
 
         Schema::create('persona',function(Blueprint $table){
             $table->increments('idpersona');
             $table->string('tipo_persona');
             $table->string('nombre');
-            $table->string('tipo_documento');
-            $table->string('num_documento');
-            $table->string('direccion');
-            $table->string('telefono');
-            $table->string('email');
-            $table->integer('idcategoria_persona')->unsigned();
+            $table->string('tipo_documento')->nullable();
+            $table->string('num_documento')->nullable();
+            $table->string('direccion')->nullable();
+            $table->string('telefono')->nullable();
+            $table->string('email')->nullable();
+            $table->integer('idcategoria_persona')->unsigned()->nullable();
         });
         Schema::table('persona', function($table){
             $table->foreign('idcategoria_persona')->references('idcategoria_persona')->on('categoria_persona');
@@ -43,14 +43,13 @@ class Tables extends Migration
         Schema::create('articulo', function (Blueprint $table) {
             $table->increments('idarticulo');
             $table->integer('idcategoria')->unsigned();
-            $table->string('codigo');
+            $table->string('codigo')->nullable();
             $table->string('nombre');
-            $table->integer('stock');
-            $table->string('descripcion');
-            $table->string('imagen');
-            $table->string('estado');
-            $table->integer('minStock');
-            $table->tinyInteger('condicion');
+            $table->integer('stock')->nullable();
+            $table->string('descripcion')->nullable();
+            $table->string('imagen')->nullable();
+            $table->string('estado')->nullable();
+            $table->integer('minStock')->nullable();
         });
         Schema::table('articulo', function($table){
             $table->foreign('idcategoria')->references('idcategoria')->on('categoria');
@@ -59,13 +58,13 @@ class Tables extends Migration
         Schema::create('venta', function (Blueprint $table) {
             $table->increments('idventa');
             $table->integer('idcliente')->unsigned();
-            $table->string('tipo_comprobante');
-            $table->string('serie_comprobante');
-            $table->string('num_comprobante');
-            $table->dateTime('fecha_hora');
-            $table->decimal('total_venta',11,2);
-            $table->decimal('impuesto',4,2);
-            $table->string('estado');
+            $table->string('tipo_comprobante')->nullable();
+            $table->string('serie_comprobante')->nullable();
+            $table->string('num_comprobante')->nullable();
+            $table->dateTime('fecha_hora')->nullable();
+            $table->decimal('total_venta',11,2)->nullable();
+            $table->decimal('impuesto',4,2)->nullable();
+            $table->string('estado')->nullable();
         });
 
         Schema::table('venta', function($table){
@@ -76,10 +75,10 @@ class Tables extends Migration
             $table->increments('iddetalle_venta');
             $table->integer('idventa')->unsigned();
             $table->integer('idarticulo')->unsigned();
-            $table->integer('cantidad');
-            $table->decimal('precio_compra',4,2);
-            $table->integer('mano_obra');
-            $table->decimal('descuento',4,2);
+            $table->integer('cantidad')->nullable();
+            $table->decimal('precio_compra',4,2)->nullable();
+            $table->integer('mano_obra')->nullable();
+            $table->decimal('descuento',4,2)->nullable();
         });
 
         Schema::table('detalle_venta',function($table){
@@ -93,11 +92,11 @@ class Tables extends Migration
             $table->increments('idingreso');
             $table->integer('idproveedor')->unsigned();
             $table->string('tipo_comprobante');
-            $table->string('serie_comprobante');
-            $table->string('num_comprobante');
-            $table->dateTime('fecha_hora');
-            $table->decimal('impuesto',4,2);
-            $table->string('estado');
+            $table->string('serie_comprobante')->nullable();
+            $table->string('num_comprobante')->nullable();
+            $table->dateTime('fecha_hora')->nullable();
+            $table->decimal('impuesto',4,2)->nullable();
+            $table->string('estado')->nullable();
         });
 
         Schema::table('ingreso', function($table){
@@ -108,9 +107,9 @@ class Tables extends Migration
             $table->increments('iddetalle_ingreso');
             $table->integer('idingreso')->unsigned();
             $table->integer('idarticulo')->unsigned();
-            $table->integer('cantidad');
-            $table->decimal('precio_compra',4,2);
-            $table->decimal('precio_venta',4,2);
+            $table->integer('cantidad')->nullable();
+            $table->decimal('precio_compra',4,2)->nullable();
+            $table->decimal('precio_venta',4,2)->nullable();
         });
 
         Schema::table('detalle_ingreso',function($table){
